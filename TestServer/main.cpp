d4b3a0f8e64050d1e14009724b5863f6382c74ac
freeze325 <wuqunyongproxy@gmail.com>
2020-05-28 09:26:25 +0800

    no message

--- TestServer/main.cpp
+++ TestServer/main.cpp
@@ -1,31 +1,13 @@
-#include "network/windows_platform.h"
-
 #include <cstdlib>
 #include <string>
 #include <iostream>
 #include <map>
 #include <vector>
 #include <algorithm>
-
-
 #include <stdio.h>
 #include <locale.h>
-#include <time.h>
 
-#include "yaml-cpp/yaml.h"
-
-#include "event/real_time_system.h"
-#include "event/dispatched_thread.h"
-#include "event/libevent_scheduler.h"
-
-#include "api/api_impl.h"
-#include "api/pb_handler.h"
-
-#include "network/platform_impl.h"
-#include "network/Ctx.h"
-#include "network/output_stream.h"
-
-#include "google/protobuf/message.h"
+#include "apie.h"
 
 #include "../PBMsg/login_msg.pb.h"
 
