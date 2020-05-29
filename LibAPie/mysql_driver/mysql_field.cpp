7ee4cd8b01b95a10e6649c415cbcba711839aec8
freeze325 <wuqunyongproxy@gmail.com>
2020-05-29 14:14:00 +0800

    no message

--- LibAPie/mysql_driver/mysql_field.cpp
+++ LibAPie/mysql_driver/mysql_field.cpp
@@ -40,3 +40,67 @@ uint32_t MysqlField::getOffset()
 {
 	return this->m_offset;
 }
+
+MysqlField::DB_FIELD_TYPE MysqlField::convertType()
+{
+	DB_FIELD_TYPE fieldType;
+	switch (m_type)
+	{
+	case MYSQL_TYPE_TINY_BLOB:
+	case MYSQL_TYPE_MEDIUM_BLOB:
+	case MYSQL_TYPE_LONG_BLOB:
+	case MYSQL_TYPE_BLOB:
+		fieldType = DB_FIELD_TYPE::T_BYTES;
+		break;
+
+	case FIELD_TYPE_STRING:
+	case FIELD_TYPE_VAR_STRING:
+	case FIELD_TYPE_DATETIME:
+		fieldType = DB_FIELD_TYPE::T_STRING;
+		break;
+
+	case FIELD_TYPE_FLOAT:
+		fieldType = DB_FIELD_TYPE::T_FLOAT;
+		break;
+
+	case FIELD_TYPE_DOUBLE:
+		fieldType = DB_FIELD_TYPE::T_DOUBLE;
+		break;
+
+	case FIELD_TYPE_TINY:
+		fieldType = DB_FIELD_TYPE::T_INT8;
+		break;
+
+	case FIELD_TYPE_SHORT:
+		fieldType = DB_FIELD_TYPE::T_INT16;
+		break;
+
+	case FIELD_TYPE_LONG:
+		fieldType = DB_FIELD_TYPE::T_INT32;
+		break;
+
+	case FIELD_TYPE_LONGLONG:
+		fieldType = DB_FIELD_TYPE::T_INT64;
+		break;
+
+	case MYSQL_TYPE_NEWDECIMAL:
+		fieldType = DB_FIELD_TYPE::T_INT64;
+		break;
+
+	default:
+		fieldType = DB_FIELD_TYPE::T_NONE;
+		break;
+	}
+
+	if (fieldType == DB_FIELD_TYPE::T_NONE)
+	{
+		return fieldType;
+	}
+
+	if (is_unsigned())
+	{
+		fieldType = DB_FIELD_TYPE(uint32_t(fieldType) | 0x20);
+	}
+
+	return fieldType;
+}
