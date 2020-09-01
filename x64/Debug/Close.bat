853a5a7ba1facfc2de42b51ae5c8eba417cd1514
freeze325 <wuqunyongproxy@gmail.com>
2020-09-01 14:19:58 +0800

    no message

---
+++ x64/Debug/Close.bat
@@ -0,0 +1,14 @@
+﻿@echo off
+set curdir=%cd%
+
+set var=DBProxy.exe GatewayServer.exe RouteProxy.exe SceneServer.exe ServiceRegistry.exe
+for %%a in (%var%) do (
+	tasklist /nh | findstr /i %%a > NUL  
+	if ErrorLevel 0 (
+	  taskkill /f /t /im "%%a"
+	) else (
+	  echo not exist %%a
+	)
+)
+
+pause
---
+++ x64/Debug/Start.bat
@@ -0,0 +1,20 @@
+
+call :startfunc DBProxy.exe ../../conf/db_proxy.yaml
+call :startfunc GatewayServer.exe ../../conf/gateway_server.yaml
+call :startfunc RouteProxy.exe ../../conf/route_proxy.yaml
+call :startfunc SceneServer.exe ../../conf/scene_server.yaml
+call :startfunc ServiceRegistry.exe ../../conf/service_registry.yaml
+
+goto end
+
+:startfunc
+	tasklist /nh | findstr /i %1 > NUL  
+	if ErrorLevel 1 (
+	  start %1 %2
+	) else (
+	  echo exist %1 %2
+	)
+	
+goto :eof
+
+:end
