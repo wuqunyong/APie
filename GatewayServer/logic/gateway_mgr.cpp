2c1bb34689e70244f1a066c1e166c3b0d7b9b880
freeze325 <wuqunyongproxy@gmail.com>
2020-12-10 14:55:47 +0800

    no message

--- GatewayServer/logic/gateway_mgr.cpp
+++ GatewayServer/logic/gateway_mgr.cpp
@@ -20,6 +20,7 @@ std::tuple<uint32_t, std::string> GatewayMgr::init()
 	LogicCmdHandlerSingleton::get().registerOnCmd("load_from_db", "mysql_load_from_db_orm", GatewayMgr::onMysqlLoadFromDbORM);
 	LogicCmdHandlerSingleton::get().registerOnCmd("query_from_db", "mysql_query_from_db_orm", GatewayMgr::onMysqlQueryFromDbORM);
 	
+
 	// RPC
 	APie::RPC::rpcInit();
 	APie::RPC::RpcServerSingleton::get().registerOpcodes<::rpc_msg::PRC_DeMultiplexer_Forward_Args>(rpc_msg::RPC_DeMultiplexer_Forward, GatewayMgr::RPC_handleDeMultiplexerForward);
@@ -205,299 +206,301 @@ void GatewayMgr::onLogicCommnad(uint64_t topic, ::google::protobuf::Message& msg
 
 	handlerOpt.value()(command);
 
-	//static std::map<std::string, MysqlTable> loadedTable;
-	//static ModelUser loadedUser;
+	/*
+	static std::map<std::string, MysqlTable> loadedTable;
+	static ModelUser loadedUser;
 
-	//if (command.cmd() == "mysql_desc")
-	//{
-	//	::mysql_proxy_msg::MysqlDescribeRequest args;
+	if (command.cmd() == "mysql_desc")
+	{
+		::mysql_proxy_msg::MysqlDescribeRequest args;
 
-	//	if (command.params_size() < 2)
-	//	{
-	//		return;
-	//	}
+		if (command.params_size() < 2)
+		{
+			return;
+		}
 
-	//	std::string tableName = command.params()[0];
-	//	uint64_t userId = std::stoull(command.params()[1]);
+		std::string tableName = command.params()[0];
+		uint64_t userId = std::stoull(command.params()[1]);
 
-	//	auto ptrAdd = args.add_names();
-	//	*ptrAdd = tableName;
+		auto ptrAdd = args.add_names();
+		*ptrAdd = tableName;
 
-	//	::rpc_msg::CHANNEL server;
-	//	server.set_type(common::EPT_DB_Proxy);
-	//	server.set_id(1);
+		::rpc_msg::CHANNEL server;
+		server.set_type(common::EPT_DB_Proxy);
+		server.set_id(1);
 
-	//	auto rpcCB = [tableName, userId](const rpc_msg::STATUS& status, const std::string& replyData)
-	//	{
-	//		if (status.code() != ::rpc_msg::CODE_Ok)
-	//		{
-	//			return;
-	//		}
+		auto rpcCB = [tableName, userId](const rpc_msg::STATUS& status, const std::string& replyData)
+		{
+			if (status.code() != ::rpc_msg::CODE_Ok)
+			{
+				return;
+			}
 
-	//		::mysql_proxy_msg::MysqlDescribeResponse response;
-	//		if (!response.ParseFromString(replyData))
-	//		{
-	//			return;
-	//		}
+			::mysql_proxy_msg::MysqlDescribeResponse response;
+			if (!response.ParseFromString(replyData))
+			{
+				return;
+			}
 
-	//		std::stringstream ss;
-	//		ss << response.ShortDebugString();
-	//		ASYNC_PIE_LOG("mysql_desc", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
+			std::stringstream ss;
+			ss << response.ShortDebugString();
+			ASYNC_PIE_LOG("mysql_desc", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
 
-	//		ModelUser user;
+			ModelUser user;
 
-	//		auto roleDesc = (*response.mutable_tables())[tableName];
-	//		MysqlTable table;
-	//		table = DeclarativeBase::convertFrom(roleDesc);
-	//		user.initMetaData(table);
-	//		bool bResult = user.checkInvalid();
-	//		if (bResult)
-	//		{
-	//			loadedTable[tableName] = table;
+			auto roleDesc = (*response.mutable_tables())[tableName];
+			MysqlTable table;
+			table = DeclarativeBase::convertFrom(roleDesc);
+			user.initMetaData(table);
+			bool bResult = user.checkInvalid();
+			if (bResult)
+			{
+				loadedTable[tableName] = table;
 
-	//			DAOFactoryTypeSingleton::get().addLoadedTable(DeclarativeBase::DBType::DBT_Role, tableName, table);
-	//		}
+				DAOFactoryTypeSingleton::get().addLoadedTable(DeclarativeBase::DBType::DBT_Role, tableName, table);
+			}
 
-	//		user.fields.user_id = userId;
+			user.fields.user_id = userId;
 
-	//		mysql_proxy_msg::MysqlQueryRequest queryRequest;
-	//		queryRequest = user.generateQuery();
+			mysql_proxy_msg::MysqlQueryRequest queryRequest;
+			queryRequest = user.generateQuery();
 
-	//		::rpc_msg::CHANNEL server;
-	//		server.set_type(common::EPT_DB_Proxy);
-	//		server.set_id(1);
+			::rpc_msg::CHANNEL server;
+			server.set_type(common::EPT_DB_Proxy);
+			server.set_id(1);
 
-	//		auto queryCB = [user](const rpc_msg::STATUS& status, const std::string& replyData) mutable
-	//		{
-	//			if (status.code() != ::rpc_msg::CODE_Ok)
-	//			{
-	//				return;
-	//			}
+			auto queryCB = [user](const rpc_msg::STATUS& status, const std::string& replyData) mutable
+			{
+				if (status.code() != ::rpc_msg::CODE_Ok)
+				{
+					return;
+				}
 
-	//			::mysql_proxy_msg::MysqlQueryResponse response;
-	//			if (!response.ParseFromString(replyData))
-	//			{
-	//				return;
-	//			}
+				::mysql_proxy_msg::MysqlQueryResponse response;
+				if (!response.ParseFromString(replyData))
+				{
+					return;
+				}
 
-	//			std::stringstream ss;
-	//			ss << response.ShortDebugString();
-	//			ASYNC_PIE_LOG("mysql_query", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
+				std::stringstream ss;
+				ss << response.ShortDebugString();
+				ASYNC_PIE_LOG("mysql_query", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
 
-	//			//bool bResult = user.loadFromPb(response);
-	//			//if (bResult)
-	//			//{
-	//			//	loadedUser = user;
-	//			//}
+				//bool bResult = user.loadFromPb(response);
+				//if (bResult)
+				//{
+				//	loadedUser = user;
+				//}
 
-	//			bool bResult = user.loadFromPbCheck(response);
-	//			if (!bResult)
-	//			{
-	//				return;
-	//			}
+				bool bResult = user.loadFromPbCheck(response);
+				if (!bResult)
+				{
+					return;
+				}
 
-	//			uint32_t iRowCount = response.table().rows_size();
-	//			for (auto& rowData : response.table().rows())
-	//			{
-	//				user.loadFromPb(rowData);
+				uint32_t iRowCount = response.table().rows_size();
+				for (auto& rowData : response.table().rows())
+				{
+					user.loadFromPb(rowData);
 
-	//				loadedUser = user;
-	//				break;
-	//			}
-	//		};
-	//		APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlQuery, queryRequest, queryCB);
-	//	};
-	//	APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlDescTable, args, rpcCB);
-	//}
-	//else if (command.cmd() == "mysql_insert")
-	//{
-	//	if (command.params_size() < 2)
-	//	{
-	//		return;
-	//	}
+					loadedUser = user;
+					break;
+				}
+			};
+			APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlQuery, queryRequest, queryCB);
+		};
+		APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlDescTable, args, rpcCB);
+	}
+	else if (command.cmd() == "mysql_insert")
+	{
+		if (command.params_size() < 2)
+		{
+			return;
+		}
 
-	//	std::string tableName = command.params()[0];
-	//	uint64_t userId = std::stoull(command.params()[1]);
+		std::string tableName = command.params()[0];
+		uint64_t userId = std::stoull(command.params()[1]);
 
-	//	auto findIte = loadedTable.find(tableName);
-	//	if (findIte == loadedTable.end())
-	//	{
-	//		return;
-	//	}
+		auto findIte = loadedTable.find(tableName);
+		if (findIte == loadedTable.end())
+		{
+			return;
+		}
 
-	//	//ModelUser user;
-	//	loadedUser.fields.user_id = userId;
-	//	loadedUser.initMetaData(findIte->second);
-	//	bool bResult = loadedUser.checkInvalid();
-	//	if (!bResult)
-	//	{
-	//		return;
-	//	}
+		//ModelUser user;
+		loadedUser.fields.user_id = userId;
+		loadedUser.initMetaData(findIte->second);
+		bool bResult = loadedUser.checkInvalid();
+		if (!bResult)
+		{
+			return;
+		}
 
-	//	mysql_proxy_msg::MysqlInsertRequest insertRequest = loadedUser.generateInsert();
+		mysql_proxy_msg::MysqlInsertRequest insertRequest = loadedUser.generateInsert();
 
-	//	::rpc_msg::CHANNEL server;
-	//	server.set_type(common::EPT_DB_Proxy);
-	//	server.set_id(1);
+		::rpc_msg::CHANNEL server;
+		server.set_type(common::EPT_DB_Proxy);
+		server.set_id(1);
 
-	//	auto insertCB = [](const rpc_msg::STATUS& status, const std::string& replyData) mutable
-	//	{
-	//		if (status.code() != ::rpc_msg::CODE_Ok)
-	//		{
-	//			return;
-	//		}
+		auto insertCB = [](const rpc_msg::STATUS& status, const std::string& replyData) mutable
+		{
+			if (status.code() != ::rpc_msg::CODE_Ok)
+			{
+				return;
+			}
 
-	//		::mysql_proxy_msg::MysqlInsertResponse response;
-	//		if (!response.ParseFromString(replyData))
-	//		{
-	//			return;
-	//		}
+			::mysql_proxy_msg::MysqlInsertResponse response;
+			if (!response.ParseFromString(replyData))
+			{
+				return;
+			}
 
-	//		std::stringstream ss;
-	//		ss << response.ShortDebugString();
-	//		ASYNC_PIE_LOG("mysql_insert", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
+			std::stringstream ss;
+			ss << response.ShortDebugString();
+			ASYNC_PIE_LOG("mysql_insert", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
 
-	//	};
-	//	APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlInsert, insertRequest, insertCB);
-	//}
-	//else if (command.cmd() == "mysql_update")
-	//{
-	//	if (command.params_size() < 2)
-	//	{
-	//		return;
-	//	}
+		};
+		APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlInsert, insertRequest, insertCB);
+	}
+	else if (command.cmd() == "mysql_update")
+	{
+		if (command.params_size() < 2)
+		{
+			return;
+		}
 
-	//	std::string tableName = command.params()[0];
-	//	uint64_t userId = std::stoull(command.params()[1]);
+		std::string tableName = command.params()[0];
+		uint64_t userId = std::stoull(command.params()[1]);
 
-	//	auto findIte = loadedTable.find(tableName);
-	//	if (findIte == loadedTable.end())
-	//	{
-	//		return;
-	//	}
+		auto findIte = loadedTable.find(tableName);
+		if (findIte == loadedTable.end())
+		{
+			return;
+		}
 
-	//	//ModelUser user;
-	//	loadedUser.fields.user_id = userId;
-	//	loadedUser.initMetaData(findIte->second);
-	//	bool bResult = loadedUser.checkInvalid();
-	//	if (!bResult)
-	//	{
-	//		return;
-	//	}
+		//ModelUser user;
+		loadedUser.fields.user_id = userId;
+		loadedUser.initMetaData(findIte->second);
+		bool bResult = loadedUser.checkInvalid();
+		if (!bResult)
+		{
+			return;
+		}
 
-	//	loadedUser.dirtySet();
-	//	mysql_proxy_msg::MysqlUpdateRequest insertRequest = loadedUser.generateUpdate();
+		loadedUser.dirtySet();
+		mysql_proxy_msg::MysqlUpdateRequest insertRequest = loadedUser.generateUpdate();
 
-	//	::rpc_msg::CHANNEL server;
-	//	server.set_type(common::EPT_DB_Proxy);
-	//	server.set_id(1);
+		::rpc_msg::CHANNEL server;
+		server.set_type(common::EPT_DB_Proxy);
+		server.set_id(1);
 
-	//	auto insertCB = [](const rpc_msg::STATUS& status, const std::string& replyData) mutable
-	//	{
-	//		if (status.code() != ::rpc_msg::CODE_Ok)
-	//		{
-	//			return;
-	//		}
+		auto insertCB = [](const rpc_msg::STATUS& status, const std::string& replyData) mutable
+		{
+			if (status.code() != ::rpc_msg::CODE_Ok)
+			{
+				return;
+			}
 
-	//		::mysql_proxy_msg::MysqlUpdateResponse response;
-	//		if (!response.ParseFromString(replyData))
-	//		{
-	//			return;
-	//		}
+			::mysql_proxy_msg::MysqlUpdateResponse response;
+			if (!response.ParseFromString(replyData))
+			{
+				return;
+			}
 
-	//		std::stringstream ss;
-	//		ss << response.ShortDebugString();
-	//		ASYNC_PIE_LOG("mysql_update", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
+			std::stringstream ss;
+			ss << response.ShortDebugString();
+			ASYNC_PIE_LOG("mysql_update", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
 
-	//	};
-	//	APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlUpdate, insertRequest, insertCB);
-	//}
-	//else if (command.cmd() == "mysql_delete")
-	//{
-	//	if (command.params_size() < 2)
-	//	{
-	//		return;
-	//	}
+		};
+		APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlUpdate, insertRequest, insertCB);
+	}
+	else if (command.cmd() == "mysql_delete")
+	{
+		if (command.params_size() < 2)
+		{
+			return;
+		}
 
-	//	std::string tableName = command.params()[0];
-	//	uint64_t userId = std::stoull(command.params()[1]);
+		std::string tableName = command.params()[0];
+		uint64_t userId = std::stoull(command.params()[1]);
 
-	//	auto findIte = loadedTable.find(tableName);
-	//	if (findIte == loadedTable.end())
-	//	{
-	//		return;
-	//	}
+		auto findIte = loadedTable.find(tableName);
+		if (findIte == loadedTable.end())
+		{
+			return;
+		}
 
-	//	//ModelUser user;
-	//	loadedUser.fields.user_id = userId;
-	//	loadedUser.initMetaData(findIte->second);
-	//	bool bResult = loadedUser.checkInvalid();
-	//	if (!bResult)
-	//	{
-	//		return;
-	//	}
+		//ModelUser user;
+		loadedUser.fields.user_id = userId;
+		loadedUser.initMetaData(findIte->second);
+		bool bResult = loadedUser.checkInvalid();
+		if (!bResult)
+		{
+			return;
+		}
 
-	//	mysql_proxy_msg::MysqlDeleteRequest insertRequest = loadedUser.generateDelete();
+		mysql_proxy_msg::MysqlDeleteRequest insertRequest = loadedUser.generateDelete();
 
-	//	::rpc_msg::CHANNEL server;
-	//	server.set_type(common::EPT_DB_Proxy);
-	//	server.set_id(1);
+		::rpc_msg::CHANNEL server;
+		server.set_type(common::EPT_DB_Proxy);
+		server.set_id(1);
 
-	//	auto insertCB = [](const rpc_msg::STATUS& status, const std::string& replyData) mutable
-	//	{
-	//		if (status.code() != ::rpc_msg::CODE_Ok)
-	//		{
-	//			return;
-	//		}
+		auto insertCB = [](const rpc_msg::STATUS& status, const std::string& replyData) mutable
+		{
+			if (status.code() != ::rpc_msg::CODE_Ok)
+			{
+				return;
+			}
 
-	//		::mysql_proxy_msg::MysqlDeleteResponse response;
-	//		if (!response.ParseFromString(replyData))
-	//		{
-	//			return;
-	//		}
+			::mysql_proxy_msg::MysqlDeleteResponse response;
+			if (!response.ParseFromString(replyData))
+			{
+				return;
+			}
 
-	//		std::stringstream ss;
-	//		ss << response.ShortDebugString();
-	//		ASYNC_PIE_LOG("mysql_delete", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
+			std::stringstream ss;
+			ss << response.ShortDebugString();
+			ASYNC_PIE_LOG("mysql_delete", PIE_CYCLE_DAY, PIE_ERROR, ss.str().c_str());
 
-	//	};
-	//	APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlDelete, insertRequest, insertCB);
-	//}
-	//else if (command.cmd() == "multi_mysql_desc")
-	//{
-	//	if (command.params_size() < 1)
-	//	{
-	//		return;
-	//	}
+		};
+		APie::RPC::RpcClientSingleton::get().callByRoute(server, ::rpc_msg::RPC_MysqlDelete, insertRequest, insertCB);
+	}
+	else if (command.cmd() == "multi_mysql_desc")
+	{
+		if (command.params_size() < 1)
+		{
+			return;
+		}
 
-	//	std::vector<std::tuple<::rpc_msg::CHANNEL, ::rpc_msg::RPC_OPCODES, std::string>> methods;
+		std::vector<std::tuple<::rpc_msg::CHANNEL, ::rpc_msg::RPC_OPCODES, std::string>> methods;
 
-	//	::rpc_msg::CHANNEL server;
-	//	server.set_type(common::EPT_DB_Proxy);
-	//	server.set_id(1);
+		::rpc_msg::CHANNEL server;
+		server.set_type(common::EPT_DB_Proxy);
+		server.set_id(1);
 
-	//	for (const auto& items : command.params())
-	//	{
-	//		::mysql_proxy_msg::MysqlDescribeRequest args;
+		for (const auto& items : command.params())
+		{
+			::mysql_proxy_msg::MysqlDescribeRequest args;
 
-	//		std::string tableName = items;
+			std::string tableName = items;
 
-	//		auto ptrAdd = args.add_names();
-	//		*ptrAdd = tableName;
+			auto ptrAdd = args.add_names();
+			*ptrAdd = tableName;
 
-	//		methods.push_back(std::make_tuple(server, ::rpc_msg::RPC_MysqlDescTable, args.SerializeAsString()));
-	//	}
+			methods.push_back(std::make_tuple(server, ::rpc_msg::RPC_MysqlDescTable, args.SerializeAsString()));
+		}
 
-	//	auto rpcCB = [](const rpc_msg::STATUS& status, std::vector<std::tuple<rpc_msg::STATUS, std::string>> replyData)
-	//	{
-	//		if (status.code() != ::rpc_msg::CODE_Ok)
-	//		{
-	//			return;
-	//		}
+		auto rpcCB = [](const rpc_msg::STATUS& status, std::vector<std::tuple<rpc_msg::STATUS, std::string>> replyData)
+		{
+			if (status.code() != ::rpc_msg::CODE_Ok)
+			{
+				return;
+			}
 
-	//	};
-	//	APie::RPC::RpcClientSingleton::get().multiCallByRoute(methods, rpcCB);
-	//}
+		};
+		APie::RPC::RpcClientSingleton::get().multiCallByRoute(methods, rpcCB);
+	}
+	*/
 
 }
 
