5074cb99d70a807ac5ce2ebb4099326667fa8b02
freeze325 <wuqunyongproxy@gmail.com>
2021-04-13 09:25:41 +0800

    no message

--- SQL/base/create_sql.sh
+++ SQL/base/create_sql.sh
@@ -8,7 +8,7 @@ fi
 HOST="127.0.0.1"
 USER="root"
 PASSWORD="root"
-PORT=63751
+PORT=3306
 
 
 echo HOST $HOST
@@ -27,7 +27,7 @@ echo DATABASE $DATABASE
 
 
 mysql -h$HOST -P$PORT -u$USER -p$PASSWORD $DATABASE < ./apie_account.sql 2>&1 | /usr/bin/tee -a ${logFile}
-cat ./apie_account.sql | xargs echo "`date +'%Y-%m-%d %H:%M:%S'`|apie_account.sql" | /usr/bin/tee -a ${logFile}
+cat ./apie_account.sql | xargs echo "`date +'%Y-%m-%d %H:%M:%S'`|apie_account.sql|" | /usr/bin/tee -a ${logFile}
 
 # /bin/echo "`date +'%Y-%m-%d %H:%M:%S'`|apie_account.sql " >> ${logFile}
 
@@ -45,7 +45,9 @@ echo DATABASE $DATABASE
 
 
 mysql -h$HOST -P$PORT -u$USER -p$PASSWORD $DATABASE < ./apie.sql 2>&1 | /usr/bin/tee -a ${logFile}
-/bin/echo "`date +'%Y-%m-%d %H:%M:%S'`|apie.sql " >> ${logFile}
+cat ./apie.sql | xargs echo "`date +'%Y-%m-%d %H:%M:%S'`|apie.sql|" | /usr/bin/tee -a ${logFile}
+
+#/bin/echo "`date +'%Y-%m-%d %H:%M:%S'`|apie.sql " >> ${logFile}
 
 
 # USE apie_account;
